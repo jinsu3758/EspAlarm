e75bd31313fa8925ccc31856b7413519d1e20df9
Stark <stark.ssumu@gmail.com>
2018-05-21 19:08:14 +0900

    알람 추가 버튼 클릭하면 알람 화면으로 intent전환

--- app/src/main/java/com/esp/alarm/ui/alarm/list/AlarmListActivity.java
+++ app/src/main/java/com/esp/alarm/ui/alarm/list/AlarmListActivity.java
@@ -1,15 +1,29 @@
 package com.esp.alarm.ui.alarm.list;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
 
 import com.esp.alarm.R;
+import com.esp.alarm.ui.alarm.regist.AlarmRegistActivity;
 
-public class AlarmListActivity extends AppCompatActivity implements AlarmListContract.View{
+public class AlarmListActivity extends AppCompatActivity implements AlarmListContract.View {
+    private FloatingActionButton alarmAddButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_list);
+        initView();
+
+        alarmAddButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), AlarmRegistActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void initView() {
+        alarmAddButton = (FloatingActionButton) findViewById(R.id.btn_alarm_add);
     }
 }
